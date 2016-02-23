c0c6c959d2676d4a4d694450b3ce1425f8a10f34
shubham prasad <spd2192@gmail.com>
2016-02-23 17:20:06 +0530

    bug fix: radio button

--- app/scripts/controllers/registrationdetails.js
+++ app/scripts/controllers/registrationdetails.js
@@ -13,202 +13,202 @@ angular.module('frApp')
     $scope.tickets = $rootScope.InstanceTicketJson ? $rootScope.InstanceTicketJson.response : (function(){
       alert('Improper Transition, if possible start from beginning or contact customer support');
       /*
-      return [{
-        'ticketName': '7 Km',
-        'salesStartDate': 1439749800000,
-        'salesEndDate': 1459362600000,
-        'ageFrom': 20, 'ageTo': 55,
-        'ticketPrice': 500, 'discount': false,
-        'refundable': false, 'totalTickets': 500,
-        'minTicketsPerTransaction': 1,
-        'maxTicketsPerTransaction': 9,
-        'discountMaster': {}, 'ticketId': 1131,
-        'tdrId': 3,'noOfTickets': 2, 'price': 0,
-        'discountPrice': 0, 'ticketSelected':true,
-        'isDeleted': 0,
-        "participantMasterCollection": [
-          {
-            "participantId": 2040,
-            "fieldName": "First Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086453,
-            "modifiedBy": 36,
-            "participantSpecimenId": 1
-          },
-          {
-            "participantId": 2041,
-            "fieldName": "Last Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 2
-          },
-          {
-            "participantId": 2042,
-            "fieldName": "Gender",
-            "fieldType": "radio_button",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 3
-          },
-          {
-            "participantId": 2043,
-            "fieldName": "Date of Birth",
-            "fieldType": "date_picker",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 4
-          },
-          {
-            "participantId": 2044,
-            "fieldName": "Address",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 5
-          },
-          {
-            "participantId": 2045,
-            "fieldName": "Postal Code",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 6
-          }
-        ]
-      },{
-        "ticketName":"Children","ageFrom":20,"ageTo":55,
-        "ticketPrice":0.0,"discount":false,"refundable":false,
-        "totalTickets":50,"discountMaster":{},
-        "ticketId":1133,"tdrId":1,"noOfTickets":4,
-        "price":0.0,"discountPrice":0.0,"ticketSelected":true,
-        "isDeleted":0,
-        "participantMasterCollection": [
-          {
-            "participantId": 2040,
-            "fieldName": "First Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086453,
-            "modifiedBy": 36,
-            "participantSpecimenId": 1
-          },
-          {
-            "participantId": 2041,
-            "fieldName": "Last Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 2
-          },
-          {
-            "participantId": 2042,
-            "fieldName": "Gender",
-            "fieldType": "radio_button",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 3
-          },
-          {
-            "participantId": 2043,
-            "fieldName": "Date of Birth",
-            "fieldType": "date_picker",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 4
-          },
-          {
-            "participantId": 2044,
-            "fieldName": "Address",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 5
-          },
-          {
-            "participantId": 2045,
-            "fieldName": "Postal Code",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 6
-          }
-        ]
-      },{
-        "ticketName":"Child With Autism","ageFrom":20,"ageTo":55,
-        "ticketPrice":0.0,"discount":false,"refundable":false,
-        "totalTickets":50,"ticketId":1134,
-        "tdrId":1,"noOfTickets":3,"price":0.0,"discountPrice":0.0,
-        "ticketSelected":false,"isDeleted":0,
-        "participantMasterCollection": [
-          {
-            "participantId": 2040,
-            "fieldName": "First Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086453,
-            "modifiedBy": 36,
-            "participantSpecimenId": 1
-          },
-          {
-            "participantId": 2041,
-            "fieldName": "Last Name",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 2
-          },
-          {
-            "participantId": 2042,
-            "fieldName": "Gender",
-            "fieldType": "radio_button",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 3
-          },
-          {
-            "participantId": 2043,
-            "fieldName": "Date of Birth",
-            "fieldType": "date_picker",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 4
-          },
-          {
-            "participantId": 2044,
-            "fieldName": "Address",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 5
-          },
-          {
-            "participantId": 2045,
-            "fieldName": "Postal Code",
-            "fieldType": "text_box",
-            "fieldRequired": true,
-            "modifiedOn": 1439774086457,
-            "modifiedBy": 36,
-            "participantSpecimenId": 6
-          }
-        ]
-      }];
-      */
+       return [{
+       'ticketName': '7 Km',
+       'salesStartDate': 1439749800000,
+       'salesEndDate': 1459362600000,
+       'ageFrom': 20, 'ageTo': 55,
+       'ticketPrice': 500, 'discount': false,
+       'refundable': false, 'totalTickets': 500,
+       'minTicketsPerTransaction': 1,
+       'maxTicketsPerTransaction': 9,
+       'discountMaster': {}, 'ticketId': 1131,
+       'tdrId': 3,'noOfTickets': 2, 'price': 0,
+       'discountPrice': 0, 'ticketSelected':true,
+       'isDeleted': 0,
+       "participantMasterCollection": [
+       {
+       "participantId": 2040,
+       "fieldName": "First Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086453,
+       "modifiedBy": 36,
+       "participantSpecimenId": 1
+       },
+       {
+       "participantId": 2041,
+       "fieldName": "Last Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 2
+       },
+       {
+       "participantId": 2042,
+       "fieldName": "Gender",
+       "fieldType": "radio_button",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 3
+       },
+       {
+       "participantId": 2043,
+       "fieldName": "Date of Birth",
+       "fieldType": "date_picker",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 4
+       },
+       {
+       "participantId": 2044,
+       "fieldName": "Address",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 5
+       },
+       {
+       "participantId": 2045,
+       "fieldName": "Postal Code",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 6
+       }
+       ]
+       },{
+       "ticketName":"Children","ageFrom":20,"ageTo":55,
+       "ticketPrice":0.0,"discount":false,"refundable":false,
+       "totalTickets":50,"discountMaster":{},
+       "ticketId":1133,"tdrId":1,"noOfTickets":4,
+       "price":0.0,"discountPrice":0.0,"ticketSelected":true,
+       "isDeleted":0,
+       "participantMasterCollection": [
+       {
+       "participantId": 2040,
+       "fieldName": "First Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086453,
+       "modifiedBy": 36,
+       "participantSpecimenId": 1
+       },
+       {
+       "participantId": 2041,
+       "fieldName": "Last Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 2
+       },
+       {
+       "participantId": 2042,
+       "fieldName": "Gender",
+       "fieldType": "radio_button",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 3
+       },
+       {
+       "participantId": 2043,
+       "fieldName": "Date of Birth",
+       "fieldType": "date_picker",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 4
+       },
+       {
+       "participantId": 2044,
+       "fieldName": "Address",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 5
+       },
+       {
+       "participantId": 2045,
+       "fieldName": "Postal Code",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 6
+       }
+       ]
+       },{
+       "ticketName":"Child With Autism","ageFrom":20,"ageTo":55,
+       "ticketPrice":0.0,"discount":false,"refundable":false,
+       "totalTickets":50,"ticketId":1134,
+       "tdrId":1,"noOfTickets":3,"price":0.0,"discountPrice":0.0,
+       "ticketSelected":false,"isDeleted":0,
+       "participantMasterCollection": [
+       {
+       "participantId": 2040,
+       "fieldName": "First Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086453,
+       "modifiedBy": 36,
+       "participantSpecimenId": 1
+       },
+       {
+       "participantId": 2041,
+       "fieldName": "Last Name",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 2
+       },
+       {
+       "participantId": 2042,
+       "fieldName": "Gender",
+       "fieldType": "radio_button",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 3
+       },
+       {
+       "participantId": 2043,
+       "fieldName": "Date of Birth",
+       "fieldType": "date_picker",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 4
+       },
+       {
+       "participantId": 2044,
+       "fieldName": "Address",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 5
+       },
+       {
+       "participantId": 2045,
+       "fieldName": "Postal Code",
+       "fieldType": "text_box",
+       "fieldRequired": true,
+       "modifiedOn": 1439774086457,
+       "modifiedBy": 36,
+       "participantSpecimenId": 6
+       }
+       ]
+       }];
+       */
       return [];
     })();
     $scope.tickets.forEach(function(e){
@@ -238,16 +238,14 @@ angular.module('frApp')
               key: f.fieldName ,
               type: 'radio',
               templateOptions: {
-                //type: parseInputField(f.fieldType),
                 label: f.fieldName,
-                //placeholder: 'Enter '+f.fieldName
-                options: [{
-                  name:'Male',
-                  value:'male'
-                },{
-                  name:'Female',
-                  value:'female'
-                }]
+                options: (function(){
+                  var k = [];
+                  f.participantSpecimenId.participantSpecimenDetailsList.forEach(function(e){
+                    k.push({name:fieldValue, value:fieldValue});
+                  });
+                  return k;
+                })() || [{name:'male',value:'male'},{name:'female',value:'female'}]
               }
             }
           )
