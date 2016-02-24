70be256a31be5c18bebef84c8494d68cf63e00f5
shubham prasad <spd2192@gmail.com>
2016-02-24 19:26:20 +0530

    removed organiser tab

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -27,6 +27,14 @@ angular.module('frApp')
           return $sce.trustAsHtml($scope.event.htmlContent);
         }
       };
+      $scope.organiser = {
+        manager : data.response.organizerDetails.organizerName,
+        location : data.response.organizerDetails.address1 + (function(){
+          return data.response.organizerDetails.address2 ? ", " + data.response.organizerDetails.address1 : "" ;
+        })() ,
+        phone : data.response.organizerDetails.phone || "Not Available" ,
+        email : data.response.organizerDetails.emailId || "Not Available"
+      };
     });
 
 
